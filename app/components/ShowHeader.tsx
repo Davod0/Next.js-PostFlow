37a1f3447f39945f0dc323e6d9d7d43e8be61b39
Davod <davodnikoyii@gmail.com>
2024-09-17 12:52:34 +0200

    Implemented  the component ShowHeader to display a Header

---
+++ app/components/ShowHeader.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+export default function ShowHeader() {
+    return (
+        <Box sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: '#f0f0f0',
+            padding: 2, 
+        }}>
+            <Typography variant="h3" component="h1" sx={{
+                color: '#333', 
+                fontWeight: 700,
+                textAlign: 'center', 
+                maxWidth: '80%', 
+                '@media (max-width: 600px)': {
+                    fontSize: '2rem',
+                },
+            }}>
+                Welcome to my blog posts page
+            </Typography>
+        </Box>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,16 @@
 "use client"
 
-import { Typography } from "@mui/material";
 import PostCard from "./components/PostCard";
+import ShowHeader from "./components/ShowHeader";
 import { posts } from "./data";
 
 export default function Home() {
 
-    const postsList = posts;
+  const postsList = posts;
 
   return (
        <main>
-        <Typography variant="h1">Welcome to my blog posts page</Typography>
-
+        <ShowHeader/>
         {postsList.map((post) => (
           <PostCard key={post.id} post={post} />
         ))}
